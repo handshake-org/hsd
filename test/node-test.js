65839ac6320124d53831e9c83441a02630556f3c
Javed Khan <tuxcanfly@gmail.com>
2018-10-24 22:47:05 +0530

    chain: refactor indexers

--- test/node-test.js
+++ test/node-test.js
@@ -20,6 +20,8 @@ const node = new FullNode({
   apiKey: 'foo',
   network: 'regtest',
   workers: true,
+  indexTX: true,
+  indexAddress: true,
   plugins: [require('../lib/wallet/plugin')]
 });
 
@@ -653,6 +655,55 @@ describe('Node', function() {
     // assert.strictEqual(result.transactions[0].hash, tx2.txid());
     // assert.strictEqual(result.transactions[1].hash, tx1.txid());
     assert.strictEqual(result.coinbasevalue, 1000 * consensus.COIN + fees);
+  });
+
+  it('should get tx by hash', async () => {
+    const block = await mineBlock();
+    await chain.add(block);
+
+    const tx = block.txs[0];
+    const hash = tx.hash();
+    const hasTX = await node.hasTX(hash);
+
+    assert.strictEqual(hasTX, true);
+
+    const tx2 = await node.getTX(hash);
+    assert.strictEqual(tx.txid(), tx2.txid());
+
+    const meta = await node.getMeta(hash);
+    assert.strictEqual(meta.tx.txid(), tx2.txid());
+  });
+
+  it('should get coin/tx by addr', async () => {
+    const addr = await wallet.receiveAddress();
+    const mtx = await wallet.createTX({
+      rate: 100000,
+      outputs: [{
+        value: 100000,
+        address: addr
+      }]
+    });
+
+    await wallet.sign(mtx);
+
+    const tx = mtx.toTX();
+    const job = await miner.createJob();
+
+    job.addTX(tx, mtx.view);
+    job.refresh();
+
+    const block = await job.mineAsync();
+    await chain.add(block);
+
+    await new Promise(r => setTimeout(r, 300));
+
+    const txs = await node.getTXByAddress(addr.hash);
+    const tx2 = txs[0];
+    assert.strictEqual(tx.txid(), tx2.txid());
+
+    const coins = await node.getCoinsByAddress(addr.hash);
+    const coin = coins[0];
+    assert.strictEqual(tx.txid(), coin.txid());
   });
 
   it('should cleanup', async () => {
