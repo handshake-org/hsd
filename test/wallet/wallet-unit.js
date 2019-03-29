55e0f887531346e7a25af6d9ff0ef7c3a9f7c313
Mark Tyneway <mark@purse.io>
2019-03-29 13:14:33 -0700

    wallet: test constructor

--- test/wallet/wallet-unit.js
+++ test/wallet/wallet-unit.js
@@ -4,10 +4,20 @@
 'use strict';
 
 const assert = require('../util/assert');
-const common = require('../util/common');
+const blake2b = require('bcrypto/lib/blake2b');
+const {base58} = require('bstring');
 const random = require('bcrypto/lib/random');
-const {HDPrivateKey, Mnemonic, WalletDB, wallet} = require('../../lib/hsd');
+const bio = require('bufio');
+const {
+  HDPrivateKey,
+  Mnemonic,
+  WalletDB,
+  Network,
+  wallet: { Wallet }
+} = require('../../lib/hsd');
+
 const mnemonics = require('../data/mnemonic-english.json');
+const network = Network.get('main');
 
 describe('Wallet Unit Tests', () => {
   describe('constructor', () => {
@@ -15,7 +25,8 @@ describe('Wallet Unit Tests', () => {
     const phrase = mnemonics[0][1];
     const passphrase = mnemonics[0][2];
     const mnemonic = new Mnemonic(phrase, passphrase);
-    const xprv = HDPrivateKey.fromMnemonic(mnemonic).xprivkey();
+    const hdprivkey = HDPrivateKey.fromMnemonic(mnemonic);
+    const xprv = hdprivkey.xprivkey();
 
     let wdb;
 
@@ -28,7 +39,7 @@ describe('Wallet Unit Tests', () => {
     // each time wdb.create is called, it mutates properties
     // on the WalletDB instance
     beforeEach(async () => {
-      wdb = new WalletDB();
+      wdb = new WalletDB({ network: network.type });
       await wdb.open();
     });
 
@@ -37,7 +48,7 @@ describe('Wallet Unit Tests', () => {
       wdb = null;
     });
 
-    it('should handle options.master', async () => {
+    it('should handle options.master', async ($) => {
       // Should fail due to invalid key.
       const errorMsg = 'Must create wallet with hd private key.';
       const assertMsg = 'Invalid HD.PrivateKey should throw.';
@@ -47,27 +58,27 @@ describe('Wallet Unit Tests', () => {
 
       {
         // Should instatiate from String.
-        const walletdb = await wdb.create({ master: xprv });
+        const wallet = await wdb.create({ master: xprv });
         const want = xprv;
-        const got = walletdb.master.key.xprivkey();
+        const got = wallet.master.key.xprivkey();
         assert.deepEqual(got, want, 'Failed to instantiate from String.');
       }
 
       {
         // Should instatiate from HD.PrivateKey.
         const opt = { master: HDPrivateKey.fromMnemonic(mnemonic) };
-        const walletdb = await wdb.create(opt);
+        const wallet = await wdb.create(opt);
         const want = xprv;
-        const got = walletdb.master.key.xprivkey();
+        const got = wallet.master.key.xprivkey();
         assert.deepEqual(got, want, 'Failed to instatiate from HD.PrivateKey.');
       }
     });
 
     it('should handle options.mnemonic', async () => {
       // Should instantiate from HD.Mnemonic.
-      const walletdb = await wdb.create({ mnemonic: phrase });
+      const wallet = await wdb.create({ mnemonic: phrase });
       const want = phrase;
-      const got = walletdb.master.mnemonic.phrase;
+      const got = wallet.master.mnemonic.phrase;
       assert.deepEqual(got, want, 'Phrase mismatch.');
     });
 
@@ -76,24 +87,24 @@ describe('Wallet Unit Tests', () => {
         // Wallet ids increment by one staring at 1 each
         // time that a new wallet is created
         for (let i = 0; i < 3; i++) {
-          const walletdb = await wdb.create();
+          const wallet = await wdb.create();
           const want = i + 1;
-          const got = walletdb.wid;
+          const got = wallet.wid;
           assert.deepEqual(got, want, 'Wallet ID mismatch.');
         }
       }
 
       {
         // fromOptions should appropriately set value
-        const w = wallet.Wallet.fromOptions(wdb, { wid: 2 });
+        const w = Wallet.fromOptions(wdb, { wid: 2 });
         const want = 2;
         const got = w.wid;
         assert.equal(got, want, 'Wallet ID mismatch.');
       }
       {
         // Wallet ids can only be uint32s
-        assert.throws(() => wallet.Wallet.fromOptions(wdb, { wid: 2**32 }));
-        assert.throws(() => wallet.Wallet.fromOptions(wdb, { wid: -1 }));
+        assert.throws(() => Wallet.fromOptions(wdb, { wid: 2**32 }));
+        assert.throws(() => Wallet.fromOptions(wdb, { wid: -1 }));
       }
     });
 
@@ -119,32 +130,88 @@ describe('Wallet Unit Tests', () => {
           const fn = async () => await wdb.create({ id });
           assert.asyncThrows(fn, 'Bad wallet ID.');
         } else {
-          const walletdb = await wdb.create({ id });
+          const wallet = await wdb.create({ id });
 
-          const got = walletdb.id;
+          const got = wallet.id;
           const want = id;
           assert.equal(got, want);
         }
       }
+
+      {
+        // Auto generated id matches schema
+        // BLAKE2b(m/44->public|magic, 20)
+        // of `0x03be04` to base58
+        const wallet = await wdb.create({ mnemonic });
+        // hdprivkey is derived from the mnemonic
+        const key = hdprivkey.derive(44);
+
+        const bw = bio.write(37);
+        bw.writeBytes(key.publicKey);
+        bw.writeU32(network.magic);
+
+        const hash = blake2b.digest(bw.render(), 20);
+
+        const b58 = bio.write(23);
+        b58.writeU8(0x03);
+        b58.writeU8(0xbe);
+        b58.writeU8(0x04);
+        b58.writeBytes(hash);
+
+        const want = base58.encode(b58.render());
+        const got = wallet.id;
+        assert.equal(got, want);
+      }
     });
 
-    it('should handle options.watchOnly', async () => {
+    it('should handle auto generation of tokens', async () => {
+      const wallet = await wdb.create({ mnemonic });
+      // hdprivkey is derived from the mnemonic
+      const key = hdprivkey.derive(44, true);
+
+      // Always use the same privateKey
+      function getToken(nonce) {
+        const bw = bio.write(36);
+        bw.writeBytes(key.privateKey);
+        bw.writeU32(nonce);
+        return blake2b.digest(bw.render());
+      }
+
+      // Assert that the nonce is generated correctly
+      // for different integers
+      for (let i = 0; i < 3; i++) {
+        const nonce = i;
+        const want = getToken(nonce);
+        const got = wallet.getToken(nonce);
+        assert.bufferEqual(want, got);
+      }
+
       {
-        // Should create a WalletDB with a watch only account
+        // Tokens can only be generated safely for
+        // up through the MAX_SAFE_INTEGER
+        const nonce = Number.MAX_SAFE_INTEGER + 1;
+        const msg = '\'num\' must be a(n) integer';
+        assert.throws(() => wallet.getToken(nonce), msg);
+      }
+    });
+
+    it('should handle options.watchOnly', async ($) => {
+      {
+        // Should create a Wallet with a watch only account
         // and be able to recover the accountKey
-        const walletdb = await wdb.create({
+        const wallet = await wdb.create({
           watchOnly: true,
           accountKey: xpub
         });
 
         {
-          const got = walletdb.watchOnly;
+          const got = wallet.watchOnly;
           const want = true;
           assert.equal(got, want);
         }
 
         {
-          const key = await walletdb.accountKey();
+          const key = await wallet.accountKey();
           const got = key.xpubkey();
           const want = xpub;
           assert.equal(got, want);
@@ -167,18 +234,18 @@ describe('Wallet Unit Tests', () => {
     it('should handle options.accountDepth', async () => {
       {
         // fromOptions should appropriately set value
-        const w = wallet.Wallet.fromOptions(wdb, { accountDepth: 2 });
+        const w = Wallet.fromOptions(wdb, { accountDepth: 2 });
         const got = w.accountDepth;
         const want = 2;
         assert.equal(got, want, 'Account Depth mismatch.');
       }
 
       {
-        // WalletDB increments the account depth each time after
+        // Wallet increments the account depth each time after
         // creating an account
         for (let i = 0; i < 3; i++) {
-          const walletdb = await wdb.create({ accountDepth: i });
-          const got = walletdb.accountDepth;
+          const wallet = await wdb.create({ accountDepth: i });
+          const got = wallet.accountDepth;
           const want = i + 1;
           assert.equal(got, want);
         }
@@ -187,18 +254,18 @@ describe('Wallet Unit Tests', () => {
       {
         // Account Depth can only be uint32s
         const overflow = { accountDepth: 2**32 };
-        assert.throws(() => wallet.Wallet.fromOptions(wdb, overflow));
+        assert.throws(() => Wallet.fromOptions(wdb, overflow));
         const underflow = { accountDepth: -1 };
-        assert.throws(() => wallet.Wallet.fromOptions(wdb, underflow));
+        assert.throws(() => Wallet.fromOptions(wdb, underflow));
       }
     });
 
     it('should handle options.token', async () => {
       {
         const token = random.randomBytes(32);
-        const walletdb = await wdb.create({ token });
+        const wallet = await wdb.create({ token });
 
-        const got = walletdb.token;
+        const got = wallet.token;
         const want = token;
         assert.bufferEqual(got, want);
       }
@@ -213,16 +280,16 @@ describe('Wallet Unit Tests', () => {
     it('should handle options.tokenDepth', async () => {
       {
         // Token depth should be set based on the input value
-        const walletdb = await wdb.create({ tokenDepth: 10 });
-        assert.equal(walletdb.tokenDepth, 10);
+        const wallet = await wdb.create({ tokenDepth: 10 });
+        assert.equal(wallet.tokenDepth, 10);
       }
 
       {
         // Token depth can only be uint32s
         const overflow = { tokenDepth: 2**32 };
-        assert.throws(() => wallet.Wallet.fromOptions(wdb, overflow));
+        assert.throws(() => Wallet.fromOptions(wdb, overflow));
         const underflow = { tokenDepth: -1 };
-        assert.throws(() => wallet.Wallet.fromOptions(wdb, underflow));
+        assert.throws(() => Wallet.fromOptions(wdb, underflow));
       }
     });
   });
