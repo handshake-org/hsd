27c99adda0ff7f91d3ccfa0af5806cc4871d1b00
Christopher Jeffrey <chjjeffrey@gmail.com>
2019-11-22 17:05:13 -0800

    hsd: update names and faucet tree.

--- test/reserved-test.js
+++ test/reserved-test.js
@@ -5,30 +5,48 @@ const fs = require('bfile');
 const reserved = require('../lib/covenants/reserved');
 
 describe('Reserved', function() {
-  it('should get a domain', () => {
+  it('should get a top 100 domain', () => {
     const desc = reserved.getByName('twitter');
 
+    // twitter.com gets extra coins as they
+    // are one of the top domains in the world.
     assert.deepStrictEqual(desc, {
       name: 'twitter',
       hash: Buffer.from(
         '525ce500322a0f4c91070eb73829b9d96b2e70d964905fa88c8b20ea573029ea',
         'hex'),
       target: 'twitter.com.',
-      value: 755295398,
+      value: 630133159894,
       root: false
     });
   });
 
-  it('should get a reserved TLD', () => {
+  it('should get a non-top 100 domain', () => {
+    const desc = reserved.getByName('craigslist');
+
+    // Craigslist barely missed top 100 status, at rank 102.
+    assert.deepStrictEqual(desc, {
+      name: 'craigslist',
+      hash: Buffer.from(
+        '4475619b1fc842831f9af645b268fcd49b20113060f97b9fc49355a69bd0413a',
+        'hex'),
+      target: 'craigslist.org.',
+      value: 503530265,
+      root: false
+    });
+  });
+
+  it('should get a reserved TLD (also top 100)', () => {
     const desc = reserved.getByName('google');
 
+    // .google is considered a top 100 domain as they own google.com.
     assert.deepStrictEqual(desc, {
       name: 'google',
       hash: Buffer.from(
         '6292be73bdfdc4ea12bdf3018c8c553d3022b37601bb2b19153c8804bdf8da15',
         'hex'),
       target: 'google.',
-      value: 45848133594,
+      value: 660195052024,
       root: true
     });
   });
@@ -36,21 +54,45 @@ describe('Reserved', function() {
   it('should get a reserved custom name', () => {
     const desc = reserved.getByName('eth');
 
+    // .eth is reserved and passed to the ENS people for safekeeping.
     assert.deepStrictEqual(desc, {
       name: 'eth',
       hash: Buffer.from(
         '4b3cdfda85c576e43c848d43fdf8e901d8d02553fec8ee56289d10b8dc47d997',
         'hex'),
       target: 'eth.ens.domains.',
-      value: 136755295398,
+      value: 136503530265,
       root: false
+    });
+  });
+
+  it('should get an embargoed name', () => {
+    const desc = reserved.getByName('kp');
+
+    // The United States has trade embargoes against North Korea
+    // (and other nations). Although HNS is not a system of money,
+    // the protocol still avoids giving them any coins in order to
+    // avoid creating some kind of international incident.
+    //
+    //   https://www.state.gov/j/ct/rls/crt/2009/140889.htm
+    //   https://en.wikipedia.org/wiki/United_States_embargoes#Countries
+    assert.deepStrictEqual(desc, {
+      name: 'kp',
+      hash: Buffer.from(
+        '4707196b22054788dd1f05a16efb1ff54ed2ddbcd338d4bfc650e72e1829f694',
+        'hex'),
+      target: 'kp.',
+      value: 0,
+      root: true
     });
   });
 
   it('should get all names', async () => {
     const map = await fs.readJSON(`${__dirname}/../lib/covenants/names.json`);
     const zeroHash = Buffer.alloc(32, 0x00).toString('hex');
-    const [, nameValue, rootValue] = map[zeroHash];
+    // FOSS and naming projects who preferred addresses.
+    const extra = (17115975 + 10200000) * 1e6;
+    const [, nameValue, rootValue, topValue] = map[zeroHash];
     const names = [];
 
     let total = 0;
@@ -63,10 +105,17 @@ describe('Reserved', function() {
 
       const [name, flags] = item;
       const root = (flags & 1) !== 0;
-      const zero = (flags & 2) !== 0;
+      const top100 = (flags & 2) !== 0;
       const custom = (flags & 4) !== 0;
+      const zero = (flags & 8) !== 0;
 
-      let value = root ? rootValue : nameValue;
+      let value = nameValue;
+
+      if (root)
+        value += rootValue;
+
+      if (top100)
+        value += topValue;
 
       if (custom)
         value += item[2];
@@ -93,6 +142,8 @@ describe('Reserved', function() {
       assert(reserved.hasByName(item.name));
     }
 
-    assert.strictEqual(total, 203392997976906);
+    // 68000000 + 27200000 + 136000000 = 231200000
+    // 0.046457 coins are burned due to rounding.
+    assert.strictEqual(total + extra, 231199999953543);
   });
 });
