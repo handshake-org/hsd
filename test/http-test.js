354a0782ec6739f20e9254f06275cd9ae85dd80e
Christopher Jeffrey <chjjeffrey@gmail.com>
2020-02-01 23:22:27 -0800

    test/http: fix mintime.

--- test/http-test.js
+++ test/http-test.js
@@ -224,7 +224,7 @@ describe('HTTP', function() {
         + '000000000000000000000000000000000000000000000000'
         + 'ffffffffffffffffffffffffffffffffffffffffffffffff',
       curtime: json.curtime,
-      mintime: 1580305825,
+      mintime: 1580624819,
       maxtime: json.maxtime,
       expires: json.expires,
       sigoplimit: 80000,
