lif-zone/lifcoin

wallet: use correct height when adding txs

lib/wallet/http.js
@@ -873,7 +873,7 @@ class HTTP extends Server {
       if (!this.channel(name) && !this.channel('w:*'))
         return;
 
-      const json = details.toJSON(this.network, this.wdb.height);
+      const json = details.toJSON(this.network, this.wdb.liveHeight());
 
       if (this.channel(name))
         this.to(name, event, wallet.id, json);

handshake-org/hsd

lib/wallet/http.js
@@ -1297,7 +1297,7 @@ class HTTP extends Server {
       if (!this.channel(name) && !this.channel('w:*'))
         return;
 
-      const json = details.getJSON(this.network, this.wdb.height);
+      const json = details.getJSON(this.network, this.wdb.liveHeight());
 
       if (this.channel(name))
         this.to(name, event, wallet.id, json);