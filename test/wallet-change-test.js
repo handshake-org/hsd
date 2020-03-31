e22e28a98a110964bf08c60bcae88eed260a4b56
Matthew Zipkin <pinheadmz@gmail.com>
2020-03-31 11:41:19 -0400

    test: cover create change fix

---
+++ test/wallet-change-test.js
@@ -0,0 +1,104 @@
+/* eslint-env mocha */
+/* eslint prefer-arrow-callback: "off" */
+
+'use strict';
+
+const assert = require('bsert');
+const FullNode = require('../lib/node/fullnode');
+const Address = require('../lib/primitives/address');
+const {tmpdir} = require('os');
+const {randomBytes} = require('bcrypto/lib/random');
+const Path = require('path');
+
+const uniq = randomBytes(4).toString('hex');
+const path = Path.join(tmpdir(), `hsd-test-${uniq}`);
+
+const node = new FullNode({
+  prefix: path,
+  memory: false,
+  network: 'regtest',
+  plugins: [require('../lib/wallet/plugin')]
+});
+
+const {wdb} = node.require('walletdb');
+
+let wallet, recAddr;
+const changeAddrs = [];
+
+async function mineBlocks(n, addr) {
+  addr = addr ? addr : new Address().toString('regtest');
+  for (let i = 0; i < n; i++) {
+    const block = await node.miner.mineBlock(null, addr);
+    await node.chain.add(block);
+  }
+}
+
+describe('Derive and save change addresses', function() {
+  before(async () => {
+    await node.ensure();
+    await node.open();
+
+    wallet = await wdb.create();
+    recAddr = await wallet.receiveAddress();
+  });
+
+  after(async () => {
+    await node.close();
+  });
+
+  it('should fund account', async () => {
+    await mineBlocks(2, recAddr);
+
+    // Wallet rescan is an effective way to ensure that
+    // wallet and chain are synced before proceeding.
+    await wdb.rescan(0);
+
+    const aliceBal = await wallet.getBalance(0);
+    assert(aliceBal.confirmed === 2000 * 2 * 1e6);
+  });
+
+  it('should send 20 transactions', async () => {
+    for (let i = 0; i < 20; i++) {
+      const tx = await wallet.send({outputs: [{
+        address: Address.fromHash(Buffer.alloc(32, 1)),
+        value: 10000
+      }]});
+
+      for (const output of tx.outputs) {
+        if (output.value !== 10000)
+          changeAddrs.push(output.address);
+      }
+    }
+  });
+
+  it('should have incremented changeDepth by 20', async () => {
+    const info = await wallet.getAccount(0);
+    assert.strictEqual(info.changeDepth, 21);
+    assert.strictEqual(changeAddrs.length, 20);
+  });
+
+  it('should have all change addresses saved', async () => {
+    for (const addr of changeAddrs) {
+      assert(await wallet.hasAddress(addr));
+    }
+  });
+
+  it('should manually generate 20 change addresses', async () => {
+    for (let i = 0; i < 20; i++) {
+      const addr = await wallet.createChange();
+      changeAddrs.push(addr.getAddress());
+    }
+  });
+
+  it('should have incremented changeDepth by 20', async () => {
+    const info = await wallet.getAccount(0);
+    assert.strictEqual(info.changeDepth, 41);
+    assert.strictEqual(changeAddrs.length, 40);
+  });
+
+  it('should have all change addresses saved', async () => {
+    for (const addr of changeAddrs) {
+      assert(await wallet.hasAddress(addr));
+    }
+  });
+});
