7365b1ca36c86f9dfa4444974b0de2f8f3f296b7
Matthew Zipkin <pinheadmz@gmail.com>
2020-05-06 13:45:12 -0400

    dns: push _synth glue to additional section when resolving

--- test/resource-test.js
+++ test/resource-test.js
@@ -26,8 +26,13 @@ describe('Resource', function() {
         address: '127.0.0.1'
       },
       {
+        type: 'GLUE4',
+        ns: 'ns3.some-other-domain.',
+        address: '10.20.30.40'
+      },
+      {
         type: 'GLUE6',
-        ns: 'ns2.hns.',
+        ns: 'ns4.hns.',
         address: '::1'
       },
       {
@@ -58,16 +63,21 @@ describe('Resource', function() {
     const msg = res.toDNS('hns.', types.MX);
 
     assert(msg.answer.length === 0);
-    assert(msg.authority.length === 6);
-    assert(msg.additional.length === 2);
+    assert(msg.authority.length === 8);
+    // Notice that the glue for `ns3.some-other-domain.` is omitted
+    assert(msg.additional.length === 4);
 
-    const [ns1, ns2, synth4, synth6, ds, rrsig] = msg.authority;
-    const [glue4, glue6] = msg.additional;
+    const [ns1, ns2, ns3, ns4, synth4, synth6, ds, rrsig] = msg.authority;
+    const [glue4, glue6, synthA, synthAAAA] = msg.additional;
 
     assert.strictEqual(ns1.type, types.NS);
     assert.strictEqual(ns1.name, 'hns.');
     assert.strictEqual(ns2.type, types.NS);
     assert.strictEqual(ns2.name, 'hns.');
+    assert.strictEqual(ns3.type, types.NS);
+    assert.strictEqual(ns3.name, 'hns.');
+    assert.strictEqual(ns4.type, types.NS);
+    assert.strictEqual(ns4.name, 'hns.');
     assert.strictEqual(synth4.type, types.NS);
     assert.strictEqual(synth4.name, 'hns.');
     assert.strictEqual(synth6.type, types.NS);
@@ -79,7 +89,11 @@ describe('Resource', function() {
     assert.strictEqual(glue4.type, types.A);
     assert.strictEqual(glue4.name, 'ns2.hns.');
     assert.strictEqual(glue6.type, types.AAAA);
-    assert.strictEqual(glue6.name, 'ns2.hns.');
+    assert.strictEqual(glue6.name, 'ns4.hns.');
+    assert.strictEqual(synthA.type, types.A);
+    assert.strictEqual(synthA.name, '_fs0000g._synth.');
+    assert.strictEqual(synthAAAA.type, types.AAAA);
+    assert.strictEqual(synthAAAA.name, '_00000000000000000000000008._synth.');
 
     assert.strictEqual(ns1.data.ns, 'ns1.hns.');
     assert.strictEqual(ns2.data.ns, 'ns2.hns.');
@@ -88,6 +102,8 @@ describe('Resource', function() {
     assert.bufferEqual(ds.data.digest, json.records[0].digest);
     assert.strictEqual(glue4.data.address, '127.0.0.1');
     assert.strictEqual(glue6.data.address, '::1');
+    assert.strictEqual(synthA.data.address, '127.0.0.2');
+    assert.strictEqual(synthAAAA.data.address, '::2');
   });
 
   it('should synthesize an answer', () => {
