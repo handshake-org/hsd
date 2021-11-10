df1e8f22ebb2de43573c8c8c1a4367df4f50f15d
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2021-11-10 16:58:13 +0400

    test: add wallet TXRecord tests.

--- test/wallet-records-test.js
+++ test/wallet-records-test.js
@@ -5,28 +5,119 @@ const random = require('bcrypto/lib/random');
 const consensus = require('../lib/protocol/consensus');
 const Records = require('../lib/wallet/records');
 const ChainEntry = require('../lib/blockchain/chainentry');
+const TX = require('../lib/primitives/tx');
+
 const {
   ChainState,
-  BlockMeta
+  BlockMeta,
+  TXRecord
 } = Records;
+
+function getRandomChainState(marked = false) {
+  return {
+    startHeight: random.randomInt(),
+    startHash: random.randomBytes(32),
+    height: random.randomInt(),
+    marked: marked
+  };
+}
+
+function getRandomBlockMetaData() {
+  return {
+    hash: random.randomBytes(32),
+    height: random.randomInt(),
+    time: random.randomInt()
+  };
+}
+
+function getRandomTX() {
+  return new TX({
+    inputs: [{
+      prevout: {
+        hash: random.randomBytes(32),
+        index: random.randomRange(0, 100)
+      }
+    }],
+    outputs: [{
+      value: random.randomInt()
+    }]
+  });
+}
+
+function getRandomTXRecordData(genTX = false, genBlock = false) {
+  let block, tx;
+
+  if (genBlock) {
+    const blockData = getRandomBlockMetaData();
+    block = new BlockMeta(blockData.hash, blockData.height, blockData.time);
+  }
+
+  if (genTX)
+    tx = getRandomTX();
+
+  const data = {
+    height: block ? block.height : -1,
+    time: block ? block.time : 0,
+    block: block ? block.hash : null,
+    tx: tx,
+    hash: tx ? tx.hash() : null
+  };
+
+  return {data, tx, block};
+}
+
+/*
+ * These don't expect actual instances of these classes
+ * just object property checks.
+ */
+
+function compareChainState(state, expected) {
+  assert.strictEqual(state.startHeight, expected.startHeight);
+  assert.bufferEqual(state.startHash, expected.startHash);
+  assert.strictEqual(state.height, expected.height);
+  assert.strictEqual(state.marked, expected.marked);
+};
+
+function compareBlockMeta(actual, expected) {
+  assert.bufferEqual(actual.hash, expected.hash);
+  assert.strictEqual(actual.height, expected.height);
+  assert.strictEqual(actual.time, expected.time);
+}
+
+function compareTXRecord(actual, expected) {
+  if (actual.tx == null) {
+    // defaults
+    assert.strictEqual(actual.hash, null);
+
+    // expected should be the same (otherwise it's a bug in the test).
+    assert.strictEqual(actual.tx, expected.tx);
+    assert.strictEqual(actual.hash, expected.hash);
+  } else {
+    assert.bufferEqual(actual.hash, expected.hash);
+    assert.bufferEqual(actual.tx.encode(), expected.tx.encode());
+  }
+
+  if (actual.block == null) {
+    // same as actual.tx checks
+    assert.strictEqual(actual.height, -1);
+    assert.strictEqual(actual.time, 0);
+
+    assert.strictEqual(actual.block, expected.block);
+    assert.strictEqual(actual.height, expected.height);
+    assert.strictEqual(actual.time, expected.time);
+  } else {
+    assert.bufferEqual(actual.block, expected.block);
+    assert.strictEqual(actual.height, expected.height);
+    assert.strictEqual(actual.time, expected.time);
+  }
+
+  assert(typeof actual.mtime === 'number');
+  assert(actual.mtime > 0);
+  assert.strictEqual(actual.index, -1);
+}
 
 describe('Wallet Records', function() {
   describe('ChainState', function() {
-    function check(state, expected) {
-      assert.strictEqual(state.startHeight, expected.startHeight);
-      assert.bufferEqual(state.startHash, expected.startHash);
-      assert.strictEqual(state.height, expected.height);
-      assert.strictEqual(state.marked, expected.marked);
-    };
-
-    function getRandomChainState(marked = false) {
-      return {
-        startHeight: random.randomInt(),
-        startHash: random.randomBytes(32),
-        height: random.randomInt(),
-        marked: marked
-      };
-    }
     it('should encode/decode with defaults', () => {
       const state = new ChainState();
       const encoded = state.encode();
@@ -34,7 +125,7 @@ describe('Wallet Records', function() {
 
       assert.deepStrictEqual(state, decoded);
 
-      check(state, {
+      compareChainState(state, {
         startHeight: 0,
         startHash: consensus.ZERO_HASH,
         height: 0,
@@ -48,11 +139,11 @@ describe('Wallet Records', function() {
 
       let state = new ChainState();
       state.inject(data1);
-      check(state, data1);
+      compareChainState(state, data1);
 
       state = new ChainState();
       state.inject(data2);
-      check(state, data2);
+      compareChainState(state, data2);
     });
 
     it('should encode/decode', () => {
@@ -63,7 +154,7 @@ describe('Wallet Records', function() {
       const encoded = state.encode();
       const decoded = ChainState.decode(encoded);
 
-      check(decoded, state);
+      compareChainState(decoded, state);
     });
 
     it('should encode/decode with marked true', () => {
@@ -74,29 +165,15 @@ describe('Wallet Records', function() {
       const encoded = state.encode();
       const decoded = ChainState.decode(encoded);
 
-      check(decoded, state);
+      compareChainState(decoded, state);
     });
   });
 
   describe('BlockMeta', function() {
-    function check(actual, expected) {
-      assert.bufferEqual(actual.hash, expected.hash);
-      assert.strictEqual(actual.height, expected.height);
-      assert.strictEqual(actual.time, expected.time);
-    }
-
-    function getRandomBlockMetaData() {
-      return {
-        hash: random.randomBytes(32),
-        height: random.randomInt(),
-        time: random.randomInt()
-      };
-    }
-
-    it('should initialize with proper defaults', () => {
+    it('should initialize with defaults', () => {
       const meta = new BlockMeta();
 
-      check(meta, {
+      compareBlockMeta(meta, {
         hash: consensus.ZERO_HASH,
         height: -1,
         time: 0
@@ -106,7 +183,7 @@ describe('Wallet Records', function() {
     it('should initialize with params', () => {
       const data = getRandomBlockMetaData();
       const meta = new BlockMeta(data.hash, data.height, data.time);
-      check(meta, data);
+      compareBlockMeta(meta, data);
     });
 
     it('should inject data', () => {
@@ -114,7 +191,7 @@ describe('Wallet Records', function() {
       const meta = new BlockMeta();
       meta.inject(data);
 
-      check(meta, data);
+      compareBlockMeta(meta, data);
     });
 
     it('should encode/decode', () => {
@@ -125,8 +202,8 @@ describe('Wallet Records', function() {
       const encoded = meta.encode();
       const decoded = BlockMeta.decode(encoded);
 
-      check(meta, data);
-      check(decoded, meta);
+      compareBlockMeta(meta, data);
+      compareBlockMeta(decoded, meta);
     });
 
     it('should JSON encode/decode', () => {
@@ -142,7 +219,7 @@ describe('Wallet Records', function() {
         height: data.height,
         time: data.time
       });
-      check(decoded, data);
+      compareBlockMeta(decoded, data);
     });
 
     it('should return block hash', () => {
@@ -161,7 +238,119 @@ describe('Wallet Records', function() {
       entry.time = data.time;
 
       const meta = BlockMeta.fromEntry(entry);
-      check(meta, data);
+      compareBlockMeta(meta, data);
+    });
+  });
+
+  describe('TXRecord', function() {
+    const emptyBlock = {
+      height: -1,
+      block: null,
+      time: 0
+    };
+
+    const emptyTX = {
+      tx: null,
+      hash: null
+    };
+
+    it('should initialize with defaults', () => {
+      const wtx = new TXRecord();
+      const mtime = wtx.mtime;
+
+      compareTXRecord(wtx, {
+        mtime: mtime,
+        index: -1,
+        ...emptyTX,
+        ...emptyBlock,
+      });
+    });
+
+    it('should initialize w/ tx', () => {
+      const {data, tx} = getRandomTXRecordData(true);
+      const wtx = new TXRecord(tx);
+
+      compareTXRecord(wtx, data);
+    });
+
+    it('should initialize w/ tx and block', () => {
+      const {data, block, tx} = getRandomTXRecordData(true, true)
+      const wtx = new TXRecord(tx, block);
+
+      compareTXRecord(wtx, data);
+    });
+
+    it('should fail encode w/o tx', () => {
+      let err;
+      try {
+        const wtx = new TXRecord();
+        wtx.encode();
+      } catch (e) {
+        err = e;
+      }
+
+      assert(err, 'Should throw error w/o tx');
+    });
+
+    it('should encode/decode w/ tx', () => {
+      const {data, tx} = getRandomTXRecordData(true);
+
+      const wtx = new TXRecord(tx);
+      const encoded = wtx.encode();
+      const decoded = TXRecord.decode(encoded);
+
+      compareTXRecord(wtx, data);
+      compareTXRecord(decoded, data);
+    });
+
+    it('should encode/decode w/ tx and block', () => {
+      const {data, tx, block} = getRandomTXRecordData(true, true);
+
+      const wtx = new TXRecord(tx, block);
+      const encoded = wtx.encode();
+      const decoded = TXRecord.decode(encoded);
+
+      compareTXRecord(wtx, data);
+      compareTXRecord(decoded, data);
+    });
+
+    it('should initialize from TX', () => {
+      const {data, tx} = getRandomTXRecordData(true);
+      const wtx = TXRecord.fromTX(tx);
+      compareTXRecord(wtx, data);
+    });
+
+    it('should initialize from TX and Block', () => {
+      const {data, tx, block} = getRandomTXRecordData(true, true);
+      const wtx = TXRecord.fromTX(tx, block);
+      compareTXRecord(wtx, data);
+    });
+
+    it('should set and unset block', () => {
+      const {data, tx, block} = getRandomTXRecordData(true, true);
+      const wtx = TXRecord.fromTX(tx);
+
+      assert.strictEqual(wtx.getBlock(), null);
+      assert.strictEqual(wtx.getDepth(random.randomInt()), 0);
+      compareTXRecord(wtx, {
+        ...data,
+        ...emptyBlock
+      });
+
+      wtx.setBlock(block);
+      assert.strictEqual(wtx.getDepth(0), 0);
+      assert.strictEqual(wtx.getDepth(block.height), 1);
+      assert.strictEqual(wtx.getDepth(block.height + 1000), 1001);
+      compareBlockMeta(wtx.getBlock(), block);
+      compareTXRecord(wtx, data);
+
+      wtx.unsetBlock(block);
+      assert.strictEqual(wtx.getBlock(), null);
+      assert.strictEqual(wtx.getDepth(random.randomInt()), 0);
+      compareTXRecord(wtx, {
+        ...data,
+        ...emptyBlock
+      });
     });
   });
 });
