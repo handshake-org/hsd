5a46f8ae477decd3c90799a647eed401812e46e0
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2021-12-06 18:41:03 +0400

    pkg: include more file types in unicode lint.

--- scripts/unicode-lint.sh
+++ scripts/unicode-lint.sh
@@ -5,49 +5,71 @@ filter_ignore() {
 
   # Filter Mnemonic keywords
   filtered=`echo "$filtered" | grep -v '^\./lib/hd/words/\(french\|italian\|japanese\|spanish\|chinese-traditional\|chinese-simplified\)\.js$'`
+  filtered=`echo "$filtered" | grep -v '^\./test/data/mnemonic-japanese.json'`
 
   # filter cached files
   filtered=`echo "$filtered" | grep -v '^./node_modules/.cache'`
+
+  # Generated docs
+  filtered=`echo "$filtered" | grep -v '^./docs/reference'`
 
   # filter ci dependencies
   filtered=`echo "$filtered" | grep -v '^./node_modules/\(bslint\|@hns-dev/bsdoc\)'`
   filtered=`echo "$filtered" | grep -v '^./node_modules/\(qs\|is-windows\|psl\|js-yaml\|argparse\)'`
   filtered=`echo "$filtered" | grep -v '^./node_modules/\(form-data\|chalk\|jsesc\|json5\|browserslist\)'`
   filtered=`echo "$filtered" | grep -v '^./node_modules/\(istanbul-lib-source-maps\|archy\|esprima\)'`
-  filtered=`echo "$filtered" | grep -v '^./node_modules/\(foreground-child\|asynckit\|aws4\)'`
+  filtered=`echo "$filtered" | grep -v '^./node_modules/\(foreground-child\|asynckit\|aws4\|yargs\)'`
+  filtered=`echo "$filtered" | grep -v '^./node_modules/\(decamelize\|shebang-command\|camelcase\)'`
+  filtered=`echo "$filtered" | grep -v '^./node_modules/\(source-map\|cross-spawn\)'`
 
 
   echo "$filtered"
 }
 
-get_whitelist() {
-  # Copryright of Jan Schär in faye-websocket.js from node_modules
-  echo "ä"
+get_allowedlist() {
+  echo "äöλ⌊⌋≤↔“”⁻¹"
 }
 
+
+# Get file extentions:
+#   find . -iname '*.*' -exec sh -c "echo {} | sed  's|.*\.||g'" \; | sort -u
+includes="--include='*.c' \
+  --include='*.cc' \
+  --include='*.cmake' \
+  --include='*.gyp' \
+  --include='*.gypi' \
+  --include='*.h' \
+  --include='*.js'
+  "
+
 # Start
-# Check everything for blacklists.
-blacklist_matches=`grep $'[\u202A-\u202E\u2066-\u2069]' -r . --include='*.js' -l`
+# Check everything for disallowed list.
+disallowed_matches=`sh -c "grep $'[\u202A-\u202E\u2066-\u2069]' -r . -l $includes"`
 status=$?
 
 if [[ $status -eq 0 ]]; then
-  echo "Found blacklisted symbols"
+  echo "Found disallowed symbols"
   echo "More info at https://github.com/handshake-org/hsd/pull/658"
   echo "Files:"
-  for file in $blacklist_matches; do
+  for file in $dissallowed_matches; do
     echo "  $file"
   done
   exit 1
 fi
 
-# Check files only have whitelisted characters
-white_list=`get_whitelist`
-check_symbols=$'[^\u0020-\u007e\r\t'$white_list']'
-whitelist_matches=`grep "$check_symbols" -r . --include='*.js' -l`
-filtered=`filter_ignore "$whitelist_matches"`
+# Check files only have allowed characters
+allowed_list=`get_allowedlist`
+check_symbols=$'[^\u0020-\u007e\r\t\f'$allowed_list']'
+allowed_list_matches=`sh -c "grep \"$check_symbols\" -r . -l $includes"`
+
+# See the logs
+# sh -c "grep \"$check_symbols\" -r . $includes"
+
+
+filtered=`filter_ignore "$allowed_list_matches"`
 
 if [[ `echo "$filtered" | wc -l` -gt 1 ]]; then
-  echo "Found non-whitelisted symbols"
+  echo "Found not allowed symbols"
   echo "More info at https://github.com/handshake-org/hsd/pull/658"
   echo "Files:"
   for file in $filtered; do
