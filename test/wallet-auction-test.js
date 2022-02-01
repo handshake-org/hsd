dc6638e63f8c10b893c427d9b98e9e7bb39b1b77
Matthew Zipkin <pinheadmz@gmail.com>
2022-02-01 14:51:57 -0500

    test: cover wallet batching

--- test/wallet-auction-test.js
+++ test/wallet-auction-test.js
@@ -10,13 +10,17 @@ const WalletDB = require('../lib/wallet/walletdb');
 const Network = require('../lib/protocol/network');
 const rules = require('../lib/covenants/rules');
 const Address = require('../lib/primitives/address');
+const {Resource} = require('../lib/dns/resource');
 
 const network = Network.get('regtest');
 const NAME1 = rules.grindName(10, 2, network);
 const {
   treeInterval,
   biddingPeriod,
-  revealPeriod
+  revealPeriod,
+  transferLockup,
+  auctionMaturity,
+  renewalWindow
 } = network.names;
 
 const workers = new WorkerPool({
@@ -201,6 +205,283 @@ describe('Wallet Auction', function() {
       ns = await chain.db.getNameStateByName(NAME1);
       state = ns.state(chain.height, network);
       assert.strictEqual(state, states.BIDDING);
+    });
+  });
+
+  describe('Batch TXs', function() {
+    let wallet, receive;
+    const hardFee = 12345;
+
+    const name1 = rules.grindName(3, 0, network);
+    const name2 = rules.grindName(4, 0, network);
+    const name3 = rules.grindName(5, 0, network);
+    const name4 = rules.grindName(6, 0, network);
+
+    const res1 = Resource.fromJSON({records: [{type: 'TXT', txt: ['one']}]});
+    const res2 = Resource.fromJSON({records: [{type: 'TXT', txt: ['two']}]});
+    const res3 = Resource.fromJSON({records: [{type: 'TXT', txt: ['three']}]});
+    const res4 = Resource.fromJSON({records: [{type: 'TXT', txt: ['four']}]});
+
+    const mempool = [];
+    wdb.send = (tx) => {
+      mempool.push(tx);
+    };
+    wdb.getNameStatus = async (nameHash) => {
+      return chain.db.getNameStatus(nameHash, chain.height + 1);
+    };
+
+    async function mineBlocks(n) {
+      for (let i = 0; i < n; i++) {
+        const job = await cpu.createJob(chain.tip, receive);
+        while (mempool.length)
+          job.pushTX(mempool.pop());
+        job.refresh();
+
+        const block = await job.mineAsync();
+        const entry = await chain.add(block);
+        await wdb.addBlock(entry, block.txs);
+      }
+    }
+
+    before(async () => {
+      // Create wallet
+      wallet = await wdb.create();
+      receive = await wallet.receiveAddress();
+
+      // Fund wallet
+      await mineBlocks(20);
+
+      // Verify funds
+      const bal = await wallet.getBalance();
+      assert.strictEqual(bal.confirmed, 20 * 2000e6);
+    });
+
+    it('should create multiple OPENs with options', async () => {
+      const mtx = await wallet.createBatch(
+        [
+          ['OPEN', name1],
+          ['OPEN', name2],
+          ['OPEN', name3]
+        ],
+        {
+          hardFee
+        }
+      );
+
+      assert.strictEqual(mtx.outputs.length, 4);
+      let opens = 0;
+      for (const output of mtx.outputs) {
+        if (output.covenant.type === rules.types.OPEN)
+          opens++;
+      }
+      assert.strictEqual(opens, 3);
+      assert.strictEqual(mtx.getFee(mtx.view), hardFee);
+    });
+
+    it('should fail if one action is invalid: OPEN reserved', async () => {
+      await assert.rejects(
+        wallet.sendBatch(
+          [
+            ['OPEN', 'google'],
+            ['OPEN', name2],
+            ['OPEN', name3]
+          ]
+        ),
+        {message: 'Name is reserved: google.'}
+      );
+    });
+
+    it('should fail if one action is invalid: OPEN duplicated', async () => {
+      await assert.rejects(
+        wallet.sendBatch(
+          [
+            ['OPEN', name1],
+            ['OPEN', name1],
+            ['OPEN', name3]
+          ]
+        ),
+         {message: 'Duplicate name with exclusive action.'}
+       );
+    });
+
+    it('should fail if one action is invalid: BID early', async () => {
+      await assert.rejects(
+        wallet.sendBatch(
+          [
+            ['BID', name1, 1, 1],
+            ['OPEN', name2],
+            ['OPEN', name3]
+          ]
+        ),
+        {message: `Name has not reached the bidding phase yet: ${name1}.`}
+      );
+    });
+
+    it('should fail if one action is invalid: wrong arguments', async () => {
+      await assert.rejects(
+        wallet.sendBatch(
+          [
+            ['BID', name1, 21000000],
+            ['OPEN', name2],
+            ['OPEN', name3]
+          ]
+        ),
+        {message: 'Bad arguments for BID.'}
+      );
+    });
+
+    it('should fail if one action is invalid: REVEAL before bid', async () => {
+      await assert.rejects(
+        wallet.sendBatch(
+          [
+            ['REVEAL', name1],
+            ['OPEN', name2],
+            ['OPEN', name3]
+          ]
+        ),
+        {message: `Auction not found: ${name1}.`}
+      );
+    });
+
+    describe('Complete auction and diverse-action batches', function() {
+      it('3 OPENs', async () => {
+        await wallet.sendBatch(
+          [
+            ['OPEN', name1],
+            ['OPEN', name2],
+            ['OPEN', name3]
+          ]
+        );
+        await mineBlocks(treeInterval + 1);
+      });
+
+      it('4 BIDs', async () => {
+        await wallet.sendBatch(
+          [
+            ['BID', name1, 10000, 20000],
+            ['BID', name1, 10001, 20000], // self-snipe!
+            ['BID', name2, 30000, 40000],
+            ['BID', name3, 50000, 60000]
+          ]
+        );
+        await mineBlocks(biddingPeriod);
+      });
+
+      it('2 REVEALs then 1 REVEAL', async () => {
+        await wallet.sendBatch(
+          [
+            ['REVEAL', name1],
+            ['REVEAL', name2]
+          ]
+        );
+        // No "could not resolve preferred inputs" error
+        // because names are being revealed individually.
+        await wallet.sendBatch(
+          [
+            ['REVEAL', name3]
+          ]
+        );
+        await mineBlocks(revealPeriod);
+      });
+
+      it('3 REGISTERs, 1 REDEEM and 1 OPEN', async () => {
+        // Complete all 4 bids win and/or lose in one TX
+        const batch1 = await wallet.sendBatch(
+          [
+            ['OPEN', name4],
+            ['REDEEM', name1],
+            ['UPDATE', name1, res1],
+            ['UPDATE', name2, res2],
+            ['UPDATE', name3, res3]
+          ]
+        );
+
+        // Unlinked covenant (OPEN) was listed first but
+        // should be sorted last with the change output (NONE).
+        assert(!batch1.outputs[4].covenant.isLinked());
+        assert(!batch1.outputs[5].covenant.isLinked());
+        await mineBlocks(treeInterval + 1);
+      });
+
+      it('3 TRANSFERs and 1 BID', async () => {
+        // Transfer out of wallet
+        const nullAddr = new Address({
+          version: 31,
+          hash: Buffer.from([1, 2, 3])
+        });
+        await wallet.sendBatch(
+          [
+            ['TRANSFER', name1, nullAddr],
+            ['TRANSFER', name2, nullAddr],
+            ['TRANSFER', name3, nullAddr],
+            ['BID', name4, 70000, 80000]
+          ]
+        );
+
+        // True for regtest but not mainnet,
+        // should allow both REVEAL and FINALIZE
+        assert(transferLockup > biddingPeriod);
+        assert(transferLockup < (biddingPeriod + revealPeriod));
+        await mineBlocks(transferLockup);
+      });
+
+      it('1 FINALIZE, 1 CANCEL, 1 REVOKE and 1 REVEAL', async () => {
+        await wallet.sendBatch(
+          [
+            ['FINALIZE', name1],
+            ['CANCEL', name2],
+            ['REVOKE', name3],
+            ['REVEAL', name4]
+          ]
+        );
+
+        // Should allow for both REGISTER and re-open revoked name
+        assert(auctionMaturity > revealPeriod);
+        assert(auctionMaturity < renewalWindow);
+        await mineBlocks(auctionMaturity);
+      });
+
+      it('1 revoked name re-OPEN and 1 REGISTER', async () => {
+        const batch2 = await wallet.sendBatch(
+          [
+            ['OPEN', name3], // and the cycle begins again...
+            ['UPDATE', name4, res4]
+          ]
+        );
+
+        // Linked covenant (UPDATE) was listed last but should be sorted first.
+        assert.strictEqual(batch2.outputs[0].covenant.type, rules.types.REGISTER);
+        await mineBlocks(treeInterval);
+      });
+
+      it('should verify expected name properties', async () => {
+        const ns1 = await chain.db.getNameStateByName(name1);
+        const ns2 = await chain.db.getNameStateByName(name2);
+        const ns3 = await chain.db.getNameStateByName(name3);
+        const ns4 = await chain.db.getNameStateByName(name4);
+
+        assert.bufferEqual(ns1.data, res1.encode());
+        assert.bufferEqual(ns2.data, res2.encode());
+        assert.bufferEqual(ns3.data, Buffer.from([])); // revoked name data is cleared
+        assert.bufferEqual(ns4.data, res4.encode());
+
+        const coin1 = await wallet.getCoin(ns1.owner.hash, ns1.owner.index);
+        assert(!coin1); // name was transferred out of wallet
+
+        const coin2 = await wallet.getCoin(ns2.owner.hash, ns2.owner.index);
+        assert(coin2); // cancelled transfer is still in wallet
+
+        const coin3 = await wallet.getCoin(ns3.owner.hash, ns3.owner.index);
+        assert(!coin3); // revoked name no longer in wallet
+
+        const coin4 = await wallet.getCoin(ns4.owner.hash, ns4.owner.index);
+        assert(coin4); // name was won and registered
+
+        assert(ns1.isClosed(chain.height, network));
+        assert(ns2.isClosed(chain.height, network));
+        assert(ns3.isOpening(chain.height, network));
+        assert(ns4.isClosed(chain.height, network));
+      });
     });
   });
 });
