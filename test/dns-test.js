2b6e74b42e7b801c056762738fedb7916fb54fb0
Matthew Zipkin <pinheadmz@gmail.com>
2022-08-19 12:05:00 -0400

    test: refactor DNS test to run Full / SPV

--- test/dns-test.js
+++ test/dns-test.js
@@ -13,114 +13,64 @@ const recursiveResolver = new Resolver({timeout: 1000});
 rootResolver.setServers([`127.0.0.1:${network.nsPort}`]);
 recursiveResolver.setServers([`127.0.0.1:${network.rsPort}`]);
 
-describe('Server Configuration', function() {
-  describe('Full Node', function() {
-    let node;
+describe('DNS Servers', function() {
+  for (const spv of [true, false]) {
+    describe(spv ? 'SPV Node' : 'Full Node', function() {
+      const Node = spv ? SPVNode : FullNode;
+      let node;
 
-    afterEach(async () => {
-      await node.close();
-    });
+      describe('Server Configuration', function () {
+        afterEach(async () => {
+          await node.close();
+        });
 
-    it('should open full node with both DNS servers', async () => {
-      node = new FullNode({
-        memory: true,
-        network: network.type
+        it('should open full node with both DNS servers', async () => {
+          node = new Node({
+            memory: true,
+            network: network.type
+          });
+
+          await node.open();
+          const res1 = await rootResolver.resolveSoa('.');
+          assert(res1);
+          const res2 = await recursiveResolver.resolveSoa('.');
+          assert(res2);
+        });
+
+        it('should open full node with neither DNS server', async () => {
+          node = new Node({
+            memory: true,
+            network: network.type,
+            noDns: true
+          });
+
+          await node.open();
+          await assert.rejects(
+            rootResolver.resolveSoa('.'),
+            {message: 'querySoa ECONNREFUSED .'}
+          );
+          await assert.rejects(
+            recursiveResolver.resolveSoa('.'),
+            {message: 'querySoa ECONNREFUSED .'}
+          );
+        });
+
+        it('should open full node only with root name server', async () => {
+          node = new Node({
+            memory: true,
+            network: network.type,
+            noRs: true
+          });
+
+          await node.open();
+          const res1 = await rootResolver.resolveSoa('.');
+          assert(res1);
+          await assert.rejects(
+            recursiveResolver.resolveSoa('.'),
+            {message: 'querySoa ECONNREFUSED .'}
+          );
+        });
       });
-
-      await node.open();
-      const res1 = await rootResolver.resolveSoa('.');
-      assert(res1);
-      const res2 = await recursiveResolver.resolveSoa('.');
-      assert(res2);
     });
-
-    it('should open full node with neither DNS server', async () => {
-      node = new FullNode({
-        memory: true,
-        network: network.type,
-        noDns: true
-      });
-
-      await node.open();
-      await assert.rejects(
-        rootResolver.resolveSoa('.'),
-        {message: 'querySoa ECONNREFUSED .'}
-      );
-      await assert.rejects(
-        recursiveResolver.resolveSoa('.'),
-        {message: 'querySoa ECONNREFUSED .'}
-      );
-    });
-
-    it('should open full node only with root name server', async () => {
-      node = new FullNode({
-        memory: true,
-        network: network.type,
-        noRs: true
-      });
-
-      await node.open();
-      const res1 = await rootResolver.resolveSoa('.');
-      assert(res1);
-      await assert.rejects(
-        recursiveResolver.resolveSoa('.'),
-        {message: 'querySoa ECONNREFUSED .'}
-      );
-    });
-  });
-
-  describe('SPV Node', function() {
-    let node;
-
-    afterEach(async () => {
-      await node.close();
-    });
-
-    it('should open SPV node with both DNS servers', async () => {
-      node = new SPVNode({
-        memory: true,
-        network: network.type
-      });
-
-      await node.open();
-      const res1 = await rootResolver.resolveSoa('.');
-      assert(res1);
-      const res2 = await recursiveResolver.resolveSoa('.');
-      assert(res2);
-    });
-
-    it('should open SPV node with neither DNS server', async () => {
-      node = new SPVNode({
-        memory: true,
-        network: network.type,
-        noDns: true
-      });
-
-      await node.open();
-      await assert.rejects(
-        rootResolver.resolveSoa('.'),
-        {message: 'querySoa ECONNREFUSED .'}
-      );
-      await assert.rejects(
-        recursiveResolver.resolveSoa('.'),
-        {message: 'querySoa ECONNREFUSED .'}
-      );
-    });
-
-    it('should open SPV node only with root name server', async () => {
-      node = new SPVNode({
-        memory: true,
-        network: network.type,
-        noRs: true
-      });
-
-      await node.open();
-      const res1 = await rootResolver.resolveSoa('.');
-      assert(res1);
-      await assert.rejects(
-        recursiveResolver.resolveSoa('.'),
-        {message: 'querySoa ECONNREFUSED .'}
-      );
-    });
-  });
+  }
 });
