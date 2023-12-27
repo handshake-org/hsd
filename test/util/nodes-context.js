da25c3d6b961eb2d34479784b01781d921b997f2
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2023-12-27 11:46:59 +0400

    test: add standalone wallet to the NodeContext.

--- test/util/nodes-context.js
+++ test/util/nodes-context.js
@@ -19,7 +19,7 @@ class NodesContext {
   }
 
   addNode(options = {}) {
-    const index = this.nodeCtxs.length;
+    const index = this.nodeCtxs.length + 1;
 
     let seedPort = this.network.port + index - 1;
 
@@ -30,17 +30,21 @@ class NodesContext {
     const brontidePort = this.network.brontidePort + index;
     const httpPort = this.network.rpcPort + index + 100;
     const walletHttpPort = this.network.walletPort + index + 200;
+    const nsPort = this.network.nsPort + index;
+    const rsPort = this.network.rsPort + index + 100;
 
     const nodeCtx = new NodeContext({
+      listen: true,
+
       ...options,
 
       // override
       name: `node-${index}`,
       network: this.network,
-      listen: true,
-      publicHost: '127.0.0.1',
-      publicPort: port,
+      port: port,
       brontidePort: brontidePort,
+      rsPort: rsPort,
+      nsPort: nsPort,
       httpPort: httpPort,
       walletHttpPort: walletHttpPort,
       seeds: [
@@ -49,9 +53,19 @@ class NodesContext {
     });
 
     this.nodeCtxs.push(nodeCtx);
+    return nodeCtx;
   }
 
-  open() {
+  /**
+   * Open all or specific nodes.
+   * @param {Number} [index=-1] default all
+   * @returns {Promise}
+   */
+
+  open(index = -1) {
+    if (index !== -1)
+      return this.context(index).open();
+
     const jobs = [];
 
     for (const nodeCtx of this.nodeCtxs)
@@ -60,7 +74,16 @@ class NodesContext {
     return Promise.all(jobs);
   }
 
-  close() {
+  /**
+   * Close all or specific nodes.
+   * @param {Number} [index=-1] default all
+   * @returns {Promise}
+   */
+
+  close(index = -1) {
+    if (index !== -1)
+      return this.context(index).close();
+
     const jobs = [];
 
     for (const nodeCtx of this.nodeCtxs)
@@ -69,20 +92,51 @@ class NodesContext {
     return Promise.all(jobs);
   }
 
+  /**
+   * Destroy specific or all nodes. Clean up directories on the disk.
+   * @param {Number} [index=-1] default all
+   * @returns {Promise}
+   */
+
+  destroy(index = -1) {
+    if (index !== -1)
+      return this.context(index).destroy();
+
+    const jobs = [];
+
+    for (const nodeCtx of this.nodeCtxs)
+      jobs.push(nodeCtx.destroy());
+
+    return Promise.all(jobs);
+  }
+
+  /**
+   * Connect all nodes.
+   * @returns {Promise}
+   */
+
   async connect() {
     for (const nodeCtx of this.nodeCtxs) {
       await nodeCtx.node.connect();
-      await new Promise(r => setTimeout(r, 1000));
+      await nodeCtx.node.startSync();
     }
   }
 
+  /**
+   * Disconnect all nodes.
+   * @returns {Promise}
+   */
+
   async disconnect() {
     for (let i = this.nodeCtxs.length - 1; i >= 0; i--) {
-      const node = this.nodeCtxs[i];
+      const node = this.nodeCtxs[i].node;
       await node.disconnect();
-      await new Promise(r => setTimeout(r, 1000));
     }
   }
+
+  /**
+   * Start syncing.
+   */
 
   startSync() {
     for (const nodeCtx of this.nodeCtxs) {
@@ -92,32 +146,48 @@ class NodesContext {
     }
   }
 
+  /**
+   * Stop syncing.
+   */
+
   stopSync() {
     for (const nodeCtx of this.nodeCtxs)
       nodeCtx.stopSync();
   }
 
-  async generate(index, blocks) {
-    const nodeCtx = this.nodeCtxs[index];
+  /**
+   * Mine blocks.
+   * @param {Number} index
+   * @param {Number} blocks
+   * @param {String} address
+   * @param {ChainEntry} [tip=chain.tip]
+   * @returns {Promise}
+   */
 
-    assert(nodeCtx);
-
-    for (let i = 0; i < blocks; i++) {
-      const block = await nodeCtx.miner.mineBlock();
-      await nodeCtx.chain.add(block);
-    }
+  async generate(index, blocks, address, tip) {
+    return this.context(index).mineBlocks(blocks, address, tip);
   }
+
+  /**
+   * Get NodeCtx for the node.
+   * @param {Number} index
+   * @returns {NodeContext}
+   */
 
   context(index) {
-    const node = this.nodeCtxs[index];
-    assert(node);
-    return node;
-  }
-
-  height(index) {
     const nodeCtx = this.nodeCtxs[index];
     assert(nodeCtx);
-    return nodeCtx.height;
+    return nodeCtx;
+  }
+
+  /**
+   * Get height for the node.
+   * @param {Number} index
+   * @returns {Number}
+   */
+
+  height(index) {
+    return this.context(index).height;
   }
 }
 
